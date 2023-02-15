1041f22d09f953b340a17152ce3f6a79932315e5
zzh <zhouzhihao@jinshuju.net>
2023-02-15 09:27:47 +0800

    Revert "5.0.2-8.alpha-12 webpack add thread-loader"
    This reverts commit 1b4b7c97658ef0fe91a5902b7cc9d3c4c56448b1.

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -135,9 +135,6 @@ module.exports = function (webpackEnv) {
     preProcessorOptions = {}
   ) => {
     const loaders = [
-      {
-        loader: require.resolve('thread-loader'),
-      },
       isEnvDevelopment && {
         loader: require.resolve('style-loader'),
       },
@@ -318,78 +315,72 @@ module.exports = function (webpackEnv) {
           {
             test: /\.(js|mjs|jsx|ts|tsx)$/,
             include: [paths.appSrc, paths.appPackages, paths.uikitX],
-            use: [
-            {
-                loader: require.resolve('babel-loader'),
-            },
-            {
-              loader: require.resolve('babel-loader'),
-              options: {
-                customize: require.resolve(
-                    'babel-preset-react-app/webpack-overrides'
-                ),
-                presets: [
-                  [
-                    require.resolve('babel-preset-react-app'),
-                    {
-                      runtime: hasJsxRuntime ? 'automatic' : 'classic',
-                    },
-                  ],
+            loader: require.resolve('babel-loader'),
+            options: {
+              customize: require.resolve(
+                'babel-preset-react-app/webpack-overrides'
+              ),
+              presets: [
+                [
+                  require.resolve('babel-preset-react-app'),
+                  {
+                    runtime: hasJsxRuntime ? 'automatic' : 'classic',
+                  },
                 ],
-                // @remove-on-eject-begin
-                babelrc: false,
-                configFile: false,
-                // Make sure we have a unique cache identifier, erring on the
-                // side of caution.
-                // We remove this when the user ejects because the default
-                // is sane and uses Babel options. Instead of options, we use
-                // the react-scripts and babel-preset-react-app versions.
-                cacheIdentifier: getCacheIdentifier(
-                    isEnvProduction
-                        ? 'production'
-                        : isEnvDevelopment && 'development',
-                    [
-                      'babel-plugin-named-asset-import',
-                      'babel-preset-react-app',
-                      'react-dev-utils',
-                      'react-scripts',
-                    ]
-                ),
-                // @remove-on-eject-end
-                plugins: [
-                  isEnvDevelopment &&
+              ],
+              // @remove-on-eject-begin
+              babelrc: false,
+              configFile: false,
+              // Make sure we have a unique cache identifier, erring on the
+              // side of caution.
+              // We remove this when the user ejects because the default
+              // is sane and uses Babel options. Instead of options, we use
+              // the react-scripts and babel-preset-react-app versions.
+              cacheIdentifier: getCacheIdentifier(
+                isEnvProduction
+                  ? 'production'
+                  : isEnvDevelopment && 'development',
+                [
+                  'babel-plugin-named-asset-import',
+                  'babel-preset-react-app',
+                  'react-dev-utils',
+                  'react-scripts',
+                ]
+              ),
+              // @remove-on-eject-end
+              plugins: [
+                isEnvDevelopment &&
                   shouldUseReactRefresh &&
                   require.resolve('react-refresh/babel'),
-                  [
-                    'import',
-                    { libraryName: 'antd', libraryDirectory: 'lib', style: true },
-                    'antd',
-                  ],
-                  ['import', { libraryName: 'antd-mobile', style: 'css' }],
-                  [
-                    'import',
-                    { libraryName: '@gd-uikit/uikit', libraryDirectory: 'lib' },
-                    '@gd-uikit/uikit',
-                  ],
-                  [
-                    'import',
-                    {
-                      libraryName: 'components',
-                      libraryDirectory: '',
-                      camel2DashComponentName: false,
-                    },
-                    'components',
-                  ],
-                ].filter(Boolean),
-                // This is a feature of `babel-loader` for webpack (not Babel itself).
-                // It enables caching results in ./node_modules/.cache/babel-loader/
-                // directory for faster rebuilds.
-                cacheDirectory: true,
-                // See #6846 for context on why cacheCompression is disabled
-                cacheCompression: false,
-                compact: isEnvProduction,
-              },
-            }],
+                [
+                  'import',
+                  { libraryName: 'antd', libraryDirectory: 'lib', style: true },
+                  'antd',
+                ],
+                ['import', { libraryName: 'antd-mobile', style: 'css' }],
+                [
+                  'import',
+                  { libraryName: '@gd-uikit/uikit', libraryDirectory: 'lib' },
+                  '@gd-uikit/uikit',
+                ],
+                [
+                  'import',
+                  {
+                    libraryName: 'components',
+                    libraryDirectory: '',
+                    camel2DashComponentName: false,
+                  },
+                  'components',
+                ],
+              ].filter(Boolean),
+              // This is a feature of `babel-loader` for webpack (not Babel itself).
+              // It enables caching results in ./node_modules/.cache/babel-loader/
+              // directory for faster rebuilds.
+              cacheDirectory: true,
+              // See #6846 for context on why cacheCompression is disabled
+              cacheCompression: false,
+              compact: isEnvProduction,
+            },
           },
           // Process any JS outside of the app with Babel.
           // Unlike the application JS, we only compile the standard ES features.
